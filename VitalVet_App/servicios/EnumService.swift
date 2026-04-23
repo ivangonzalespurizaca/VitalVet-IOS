8e3857434f4b58d5c90ac3ca97b9188861c9e61c
Oscar Ivan Gonzales Purizaca <gonzalespurizacaoscarivan@gmail.com>
2026-04-23 08:57:20 -0500

    Registro Cliente Implementado

---
+++ VitalVet_App/servicios/EnumService.swift
@@ -0,0 +1,25 @@
+//
+//  EnunService.swift
+//  VitalVet_App
+//
+//  Created by Oscar Ivan Gonzales Purizaca on 22/04/26.
+//
+
+import UIKit
+
+class EnunService{
+    
+    let enumsGenerosURL = "https://apiveterinaria-production-238b.up.railway.app/api/public/enums/generos"
+
+    func obtenerGeneros(completion: @escaping ([String]?) -> Void) {
+        AF.request(enumsGenerosURL).responseDecodable(of: [String].self) { response in
+            switch response.result {
+            case .success(let generos):
+                completion(generos)
+            case .failure:
+                completion(nil)
+            }
+        }
+    }
+
+}
