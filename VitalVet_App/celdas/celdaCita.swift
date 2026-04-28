edc7396f29a4be0a44785bc7db81058b4311af0b
NicolasRC13 <cuentadecomodin12345@gmail.com>
2026-04-28 15:08:59 -0500

    parte de listado de solicitud de citas

---
+++ VitalVet_App/celdas/celdaCita.swift
@@ -0,0 +1,23 @@
+//
+//  celdaCita.swift
+//  VitalVet_App
+//
+//  Created by XCODE on 28/04/26.
+//
+
+import UIKit
+
+class celdaCita: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
